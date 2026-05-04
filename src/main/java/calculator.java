936f017eef2491f79c10931c5bc075335e751485
Muthu Savitha R <muthusavithar@gmail.com>
2026-05-04 22:58:50 +0530

    first commit

---
+++ src/main/java/calculator.java
@@ -0,0 +1,30 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    public int div(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+
+        System.out.println("Addition: " + c.add(10, 5));
+        System.out.println("Subtraction: " + c.sub(10, 5));
+        System.out.println("Multiplication: " + c.mul(10, 5));
+        System.out.println("Division: " + c.div(10, 5));
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Calculator App</title>
+</head>
+<body>
+    <h1>Calculator App Deployed Successfully 🚀</h1>
+</body>
+</html>
